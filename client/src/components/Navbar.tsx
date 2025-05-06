bb2317d52289def99821df1e9c987f76be164031
Ian Clark Tugon <itugon@filamer.edu.ph>
2025-05-06 13:49:02 +0800

    checking

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-             Aragon RNL
+            Tugon RNL
           </a>
           <button
             className="navbar-toggler"
